5c4e61478b2ed133a69f9b28d8ba3605711c7772
Alejandro Martín Albalah <dmwhispers551@gmail.com>
2023-10-05 02:50:04 -0400

    implemented movieBloc

--- lib/presentation/home/bloc/movie_bloc.dart
+++ lib/presentation/home/bloc/movie_bloc.dart
@@ -1,13 +1,39 @@
 import 'package:bloc/bloc.dart';
+import 'package:codeberry_movies_test/core/utils/utils_exports.dart';
+import 'package:codeberry_movies_test/domain/entities/entities_exports.dart';
+import 'package:codeberry_movies_test/domain/usecases/usecases_exports.dart';
 import 'package:equatable/equatable.dart';
+import 'package:fpdart/fpdart.dart';
 
 part 'movie_event.dart';
 part 'movie_state.dart';
+part 'movie_status.dart';
 
 class MovieBloc extends Bloc<MovieEvent, MovieState> {
-  MovieBloc() : super(MovieInitial()) {
-    on<MovieEvent>((event, emit) {
-      // TODO: implement event handler
+  final GetPopularMoviesUsecase getPopularMovies;
+
+  MovieBloc({required this.getPopularMovies}) : super(const MovieState()) {
+    on<MovieEvent>((event, emit) async {
+      emit(state.copyWith(status: MovieStatus.loading));
+//Thanks to dependency injection I can only call the usecase instance
+      final responseGetPopularMovies = await getPopularMovies(NoParams());
+      if (responseGetPopularMovies.isRight()) {
+        final popularMovies =
+            (responseGetPopularMovies as Right).value as List<Movie>?;
+
+        if (!isClosed) {
+          emit(state.copyWith(
+            status: MovieStatus.success,
+            moviesList: popularMovies,
+          ));
+        }
+      } else {
+        final failure = (responseGetPopularMovies as Left).value;
+        if (!isClosed) {
+          emit(state.copyWith(
+              failure: failure.message, status: MovieStatus.failure));
+        }
+      }
     });
   }
 }
