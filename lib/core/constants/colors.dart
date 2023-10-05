41402506527c692138cd371e986420df219dbd0e
Alejandro Martín Albalah <dmwhispers551@gmail.com>
2023-10-05 03:10:24 -0400

    added colors and theme

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 0, 121, 191);
+const kPrimaryLightColor = Color.fromARGB(255, 209, 222, 231);
+const kGreyTextColor = Color(0xFF757575);
---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:codeberry_movies_test/core/constants/constants_exports.dart';
+import 'package:flutter/material.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    scaffoldBackgroundColor: kPrimaryLightColor,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
