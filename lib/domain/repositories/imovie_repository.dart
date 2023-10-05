20725c78ee202a115c1e2950e7fc7dbf015c5926
Alejandro Martín Albalah <dmwhispers551@gmail.com>
2023-10-05 02:02:28 -0400

    fixed movieDetailsEntity, implemented movieRepository

--- lib/domain/repositories/imovie_repository.dart
+++ lib/domain/repositories/imovie_repository.dart
@@ -1,1 +1,8 @@
+import 'package:codeberry_movies_test/core/errors/failures.dart';
+import 'package:codeberry_movies_test/domain/entities/entities_exports.dart';
+import 'package:fpdart/fpdart.dart';
 
+abstract class IMovieRepository {
+  Future<Either<Failure, List<Movie>>> getPopularMovies();
+  Future<Either<Failure, MovieDetails>> getMovieDetails(String movieId);
+}
