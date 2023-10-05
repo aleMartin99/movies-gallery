10c0d61f4840f0280633976ab8c068124563f6fe
Alejandro Martín Albalah <dmwhispers551@gmail.com>
2023-10-05 03:18:52 -0400

    implemented initCore in injection_container

--- lib/core/injection_container/init_core.dart
+++ lib/core/injection_container/init_core.dart
@@ -1,30 +1,42 @@
 import 'dart:async';
+import 'package:codeberry_movies_test/data/datasources/imovie_datasource.dart';
+import 'package:codeberry_movies_test/data/datasources/movie_datasource/movie_datasource.dart';
+import 'package:codeberry_movies_test/data/repositories/movie_repository.dart';
+import 'package:codeberry_movies_test/domain/repositories/imovie_repository.dart';
+import 'package:codeberry_movies_test/domain/usecases/usecases_exports.dart';
+import 'package:codeberry_movies_test/presentation/home/bloc/movie_bloc.dart';
+import 'package:codeberry_movies_test/presentation/movie_details/bloc/movie_details_bloc.dart';
 import 'package:get_it/get_it.dart';
 
-//Favorece dependency inversion and single responsibility principles
+//Favors Dependency inversion and single responsibility principles
 
 FutureOr<void> initCore(GetIt sl) async {
-  // sl
-  // ..registerLazySingleton<IPlatformDatasource>(
-  //   () => TrelloDatasource(),
-  // )
-  // ..registerLazySingleton<IPlatformRepository>(
-  //   () => PlatformRepository(
-  //     sl<IPlatformDatasource>(),
-  //   ),
-  // )
-  // ..registerLazySingleton<GetUserUsecase>(() => GetUserUsecase(
-  //       getUser: sl<IPlatformRepository>().getUserByToken,
-  //     ))
-  // ..registerLazySingleton<GetBoardUsecase>(
-  //   () => GetBoardUsecase(
-  //     getBoards: sl<IPlatformRepository>().getBoardsByUser,
-  //   ),
-  // )
-  // ..registerLazySingleton<BoardBloc>(
-  //   () => BoardBloc(
-  //     getUserUsecase: sl<GetUserUsecase>(),
-  //     getBoardUsecase: sl<GetBoardUsecase>(),
-  //   ),
-  // );
+  sl
+    ..registerLazySingleton<IMovieDatasource>(
+      () => MovieDatasource(),
+    )
+    ..registerLazySingleton<IMovieRepository>(
+      () => MovieRepository(
+        sl<IMovieDatasource>(),
+      ),
+    )
+    ..registerLazySingleton<GetPopularMoviesUsecase>(
+      () => GetPopularMoviesUsecase(
+        getPopularMovies: sl<IMovieRepository>().getPopularMovies,
+      ),
+    )
+    ..registerLazySingleton<GetMovieDetailsUsecase>(
+        () => GetMovieDetailsUsecase(
+              getMovieDetails: sl<IMovieRepository>().getMovieDetails,
+            ))
+    ..registerLazySingleton<MovieBloc>(
+      () => MovieBloc(
+        getPopularMovies: sl<GetPopularMoviesUsecase>(),
+      ),
+    )
+    ..registerLazySingleton<MovieDetailsBloc>(
+      () => MovieDetailsBloc(
+        getMovieDetails: sl<GetMovieDetailsUsecase>(),
+      ),
+    );
 }
