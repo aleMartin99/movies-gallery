9297a72d5126692880ceee9330bce1eafccc2ee5
Alejandro Martín Albalah <dmwhispers551@gmail.com>
2023-10-05 03:01:01 -0400

    implemented MovieDetailsBloc

--- lib/presentation/movie_details/bloc/movie_details_state.dart
+++ lib/presentation/movie_details/bloc/movie_details_state.dart
@@ -1,10 +1,25 @@
 part of 'movie_details_bloc.dart';
 
-sealed class MovieDetailsState extends Equatable {
-  const MovieDetailsState();
-  
-  @override
-  List<Object> get props => [];
-}
+class MovieDetailsState extends Equatable {
+  const MovieDetailsState({
+    this.status = MovieDetailsStatus.initial,
+    this.details,
+    this.failure,
+  });
+  final MovieDetails? details;
+  final MovieDetailsStatus status;
+  final String? failure;
 
-final class MovieDetailsInitial extends MovieDetailsState {}
+  @override
+  List<Object?> get props => [status, failure, details];
+  MovieDetailsState copyWith({
+    MovieDetailsStatus? status,
+    MovieDetails? details,
+    String? failure,
+  }) {
+    return MovieDetailsState(
+        status: status ?? this.status,
+        failure: failure ?? this.failure,
+        details: details ?? this.details);
+  }
+}
---
+++ lib/presentation/movie_details/bloc/movie_details_status.dart
@@ -0,0 +1,8 @@
+part of 'movie_details_bloc.dart';
+
+enum MovieDetailsStatus {
+  initial,
+  loading,
+  success,
+  failure,
+}
